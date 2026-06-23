6c16a834df47a5cb0d121ad372751ab24dc3930a
imfakebot <Anh1412sun@gmail.com>
2026-06-23 23:01:01 +0700

    feat: initialize Spring Boot project with core domain entities and documentation structure

---
+++ src/main/java/com/tanh/datsan/DatsanApplication.java
@@ -0,0 +1,13 @@
+package com.tanh.datsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatsanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanh/datsan/DatsanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanh.datsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
